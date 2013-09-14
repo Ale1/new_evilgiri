35f6e67d89259b1ec1fb573874018844c69363b3
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-13 19:07:56 -0700

    sdfdsf

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,34 @@
 get '/' do
-  # Look in app/views/index.erb
+  
   erb :index
+end
+
+
+post '/scan' do
+start_time = Time.now 
+@treasure ={}
+(31600..31625).each do |elo_id|
+begin
+page = Nokogiri::HTML(open('http://www.elophant.com/league-of-legends/summoner/na/'+elo_id.to_s)) 
+swipe = {
+  :handle => page.css('h2').first.content,
+  :level => page.css('h3').first.content.level_scraper,
+  :record_wins => page.css('span.high').first.content.str_to_i
+} 
+@treasure[elo_id] = swipe unless swipe.empty? 
+rescue StandardError
+  next
+end
+end
+end_time = Time.now
+@timer = end_time - start_time
+@counter = @treasure.count
+erb :index
+end
+
+
+
+get '/stop' do
+
+redirect '/'
 end
---
+++ app/helpers/helpers.rb
@@ -0,0 +1,17 @@
+
+
+helpers do
+  def str_to_num
+    self.delete(',').to_i
+  end
+
+
+ def level_scraper
+   self.gsub(/\D/, "")
+    "level_scraper"
+ end
+
+
+
+
+end
